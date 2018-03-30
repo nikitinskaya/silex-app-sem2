149e928dc452f8eba65878ca14a831155d5131a2
Vera Nikitinskaya <nikitinskaya.v@gmail.com>
2018-03-30 20:58:55 +0300

    Initial example

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+  require 'vendor/autoload.php';
+
+  $app = new Silex\Application();
+
+  $app->get('/hello/{name}', function ($name) use ($app) {
+    return 'Hello '.$app->escape($name);
+  });
+
+  $app->get('/date', function () {
+    return date('r');
+  });
+
+  $app->run();
