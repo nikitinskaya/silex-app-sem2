0f71969f19dc5fdf0dae9b867b171a4a613b5e06
Vera Nikitinskaya <nikitinskaya.v@gmail.com>
2018-04-22 23:47:48 +0300

    Add 1603 part2 routes

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
   require 'vendor/autoload.php';
+  use Symfony\Component\HttpFoundation\Response;
 
   $app = new Silex\Application();
 
@@ -7,8 +8,24 @@
     return 'Hello '.$app->escape($name);
   });
 
-  $app->get('/date', function () {
-    return date('r');
+  $app->get('/', function () {
+    return '<h1>'.date("d/m/Y h:i\n").'</h1>';
+  });
+
+  $app->get('/info', function () {
+    return phpinfo();;
+  });
+
+  $app->get('/author', function () {
+    return new Response('<h4 id="author" title="GossJS">Вера Никитинская</h4>', 200, array(
+      'Access-Control-Allow-Origin' => '*',
+      'Access-Control-Allow-Methods' => 'GET,POST,DELETE',
+      'Access-Control-Allow-Headers' => 'Content-Type, Access-Control-Allow-Headers'
+    ));
+  });
+
+  $app->get('/print', function () {
+    return new Response((file_get_contents(basename(__FILE__))), 200, array('Content-type' => 'text/plain'));
   });
 
   $app->run();
