43cf00a9b154f0b3f3d3697659cca518744ed7ee
Vera Nikitinskaya <nikitinskaya.v@gmail.com>
2018-04-22 23:55:44 +0300

    Add 3003 weather route

--- index.php
+++ index.php
@@ -28,4 +28,12 @@
     return new Response((file_get_contents(basename(__FILE__))), 200, array('Content-type' => 'text/plain'));
   });
 
+  $app->get('/weather', function () {
+    $URL = "https://query.yahooapis.com/v1/public/yql?q=%20select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20%20woeid%3D%222123260%22)%20and%20u%3D'c'&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
+    $client = new GuzzleHttp\Client();
+    $response = $client->get($URL);
+    $result = json_decode($response->getBody());
+    return '<h1>'.$result->query->results->channel->item->forecast[1]->low.'</h1>';
+  });
+
   $app->run();
