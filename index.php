f64822134311309befb3b7aa5c7d2c21fd8adaea
Vera Nikitinskaya <nikitinskaya.v@gmail.com>
2018-04-25 00:42:37 +0300

    Add 16/03 part4

--- index.php
+++ index.php
@@ -1,43 +1,47 @@
 <?php
-  require 'vendor/autoload.php';
-  use Symfony\Component\HttpFoundation\Response;
+require 'vendor/autoload.php';
+use Symfony\Component\HttpFoundation\Response;
 
-  $app = new Silex\Application();
+$app = new Silex\Application();
 
-  $app->get('/hello/{name}', function ($name) use ($app) {
-    return 'Hello '.$app->escape($name);
-  });
+$app->get('/hello/{name}', function ($name) use ($app) {
+  return 'Hello '.$app->escape($name);
+});
 
-  $app->get('/', function () {
-    return '<h1>'.date("d/m/Y h:i\n").'</h1>';
-  });
+$app->get('/', function () {
+  return '<h1>'.date("d/m/Y h:i\n").'</h1>';
+});
 
-  $app->get('/info', function () {
-    return phpinfo();;
-  });
+$app->get('/info', function () {
+  return phpinfo();;
+});
 
-  $app->get('/author', function () {
-    return new Response('<h4 id="author" title="GossJS">Вера Никитинская</h4>', 200, array(
-      'Access-Control-Allow-Origin' => '*',
-      'Access-Control-Allow-Methods' => 'GET,POST,DELETE',
-      'Access-Control-Allow-Headers' => 'Content-Type, Access-Control-Allow-Headers'
-    ));
-  });
+$app->get('/author', function () {
+  return new Response('<h4 id="author" title="GossJS">Вера Никитинская</h4>', 200, array(
+    'Access-Control-Allow-Origin' => '*',
+    'Access-Control-Allow-Methods' => 'GET,POST,DELETE',
+    'Access-Control-Allow-Headers' => 'Content-Type, Access-Control-Allow-Headers'
+  ));
+});
 
-  $app->get('/print', function () {
-    return new Response((file_get_contents(basename(__FILE__))), 200, array('Content-type' => 'text/plain'));
-  });
+$app->get('/print', function () {
+  return new Response((file_get_contents(basename(__FILE__))), 200, array('Content-type' => 'text/plain'));
+});
 
-  $app->get('/weather', function () {
-    $URL = "https://query.yahooapis.com/v1/public/yql?q=%20select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20%20woeid%3D%222123260%22)%20and%20u%3D'c'&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
-    $client = new GuzzleHttp\Client();
-    $response = $client->get($URL);
-    $result = json_decode($response->getBody());
-    return '<h1>'.$result->query->results->channel->item->forecast[1]->low.'</h1>';
-  });
+$app->get('/weather', function () {
+  $URL = "https://query.yahooapis.com/v1/public/yql?q=%20select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20%20woeid%3D%222123260%22)%20and%20u%3D'c'&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
+  $client = new GuzzleHttp\Client();
+  $response = $client->get($URL);
+  $result = json_decode($response->getBody());
+  return '<h1>'.$result->query->results->channel->item->forecast[1]->low.'</h1>';
+});
 
-  $app->post('/byte', function() {
-    return ~(file_get_contents("php://input"))."\n";
-  });
+$app->post('/byte', function() {
+  return ~(file_get_contents("php://input"))."\n";
+});
 
-  $app->run();
+$app->get('/rates', function () {
+  return '<img src="/rates.php">';
+});
+
+$app->run();
--- readme.md
+++ readme.md
@@ -18,5 +18,8 @@
 
 invert a byte: `echo -e '0xa7' | curl -X POST --data-binary @- localhost:8080/byte`
 
+`/rates` at the moment, works on localhost only. Or just look at the [code](rates.php) & believe me that the graph looks like this: ![rates graph](rates.png)
+
+
 
 
